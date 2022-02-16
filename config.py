671a09037118fe52208b9750e930a062f7534852
Jerrehy <nizami.sadykhov@gmail.com>
2022-02-16 22:22:09 +0300

    init

--- config.py
+++ config.py
@@ -7,4 +7,4 @@ SECRET_KEY = os.urandom(32)
 app.config['SECRET_KEY'] = SECRET_KEY
 
 # Менять под глобальную базу данных
-conn = psycopg2.connect(dbname='test_db', user='postgres', password='Qwerty7', host='localhost')
+conn = psycopg2.connect(dbname='d9asfulh7bfmbd', user='rfmtowprcpllax', password='d630aac61a2f1304bc8d54bd3937cd8c6db1771eb5e51213ff59b05afd1277a3', host='ec2-52-204-196-4.compute-1.amazonaws.com')
